3f67a1916991d8c787388d9138422992d3ea2abb
Lyonn04 <trunghieu16082004@gmail.com>
2025-04-01 15:45:28 +0700

    Add files via upload

---
+++ Lab-05/base64/decrypt.py
@@ -0,0 +1,16 @@
+import base64
+
+def main():
+    try:
+        with open("data.txt", "r") as file:
+            encode_string = file.read().strip()
+
+        decode_bytes = base64.b64decode(encode_string)
+        decode_string = decode_bytes.decode('utf-8')
+
+        print("chuoi sau khi giai ma:", decode_string)
+    except Exception as e:
+        print("Loi:", e)
+
+if __name__ == "__main__":
+    main()
