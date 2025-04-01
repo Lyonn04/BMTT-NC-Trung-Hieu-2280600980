5fd1b30ab5e0b366e505e3b7bfb2509fe4592636
Lyonn04 <trunghieu16082004@gmail.com>
2025-04-01 15:57:07 +0700

    Add files via upload

---
+++ Lab-05/base64/encrypt.py
@@ -0,0 +1,15 @@
+import base64
+
+def main():
+    input_string = input("Nhap thong tin can ma hoa:")
+
+    encode_bytes = base64.b64encode(input_string.encode('utf-8'))
+    encode_string = encode_bytes.decode('utf-8')
+
+    with open("data.txt","w") as file:
+        file.write(encode_string)
+
+    print("Da ma hoa va luu vao file data.txt")
+
+if __name__ == "__main__":
+    main()
